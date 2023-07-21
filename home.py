15e4b481e3d93e956d06583fa0ed130f5e436b29
abhay <abhaydeep7890@gmail.com>
2023-07-21 21:14:02 +0530

    change in images

--- home.py
+++ home.py
@@ -7,6 +7,7 @@ class Home:
         self.root.geometry('800x800') # (width x height)
         self.root.title("Weather Watch")
         self.root.resizable(False,False)
+
         self.frame=Frame(self.root,bg='#333333')
         self.frame.place(x=0, y=0, height=800, width=800)
 
@@ -15,15 +16,13 @@ class Home:
 
 
         self.root.mainloop()
-
+        
+    # method to change desktop wallpaper
     def change_wallpaper(self):
         SPI_SETDESKWALLPAPER = 20
-        path = "G:\Python\Projects\images\wallpaper.jpg"
+        path = "G:\Python\Projects\images\pic.jpg"
         ctypes.windll.user32.SystemParametersInfoW(SPI_SETDESKWALLPAPER, 0, path, 0)
 
-            # if __name__ == "__main__":
-            #     path = "G:\Python\Projects\images\wallpaper.jpg"
-            #     change_wallpaper(path)
 
 
 if __name__ == '__main__':
